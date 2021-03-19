b6e32c141d428a9aff30caf930d89d5d4334a948
MSI\antti <n0suan00@students.oamk.fi>
2021-03-19 10:59:28 +0200

    backendii

---
+++ add.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+}
+
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+$amount = filter_var($input->amount,FILTER_SANITIZE_NUMBER_INT);
+
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+    $query = $db->prepare('insert into item(description, amount) values (:description, :amount)');
+    $query ->bindValue(':description',$description,PDO::PARAM_STR);
+    $query ->bindValue(':amount',$amount,PDO::PARAM_INT);
+    $query->execute();
+     
+    
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $db->lastInsertId(), 'description' => $description, 'amount' => $amount);
+    echo json_encode($data);
+} 
+catch (PDOException $pdoex) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error' => $pdoex->getMessage());
+    echo json_encode($error);
+    exit;
+}
